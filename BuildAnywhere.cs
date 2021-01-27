f7c5532fd9ea2aae0f67298d6ef3715f8b0d9375
wisnoskij <jonathonwisnoski@gmail.com>
2021-01-26 19:40:17 -0500

    Update BuildAnywhere.cs

--- BuildAnywhere.cs
+++ BuildAnywhere.cs
@@ -1,9 +1,6 @@
 using HarmonyLib;
 using System.Reflection;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class BuildAnywhere : Mod
 {
